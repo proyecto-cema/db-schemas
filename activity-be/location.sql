8fddbf9497b4795efbbe08178b43f20df726389f
Merlin Nuñez <merlinsn@gmail.com>
2021-11-28 17:34:37 -0300

    CM-212 Crear tabla de rotaciones

--- activity-be/location.sql
+++ activity-be/location.sql
@@ -1,10 +1,13 @@
 create table location
 (
-	id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+	id uuid default gen_random_uuid() not null
+		constraint location_pkey
+			primary key,
 	name varchar(50) not null,
 	size bigint,
 	description varchar(250),
-	establishment_cuig varchar(50) not null
+	establishment_cuig varchar(50) not null,
+	is_default boolean default false not null
 );
 
 alter table location owner to activity;
---
+++ activity-be/movement.sql
@@ -0,0 +1,24 @@
+/* Extension needs to be enabled to use UUID
+Needs to be installed as postgres user from inside the linux host.
+First install contrib
+sudo dnf install postgresql-contrib
+sudo -i -u postgres
+psql
+\connect activity
+*/
+CREATE EXTENSION pgcrypto;
+
+create table movement
+(
+	id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+	name varchar(50) not null,
+	description varchar(250),
+	execution_date date default now(),
+	establishment_cuig varchar(50) not null,
+	batch_name varchar(50),
+	bovine_tag varchar(50),
+	location_id uuid,
+	CONSTRAINT fk_location_id FOREIGN KEY(location_id) REFERENCES location(id)
+);
+
+alter table movement owner to activity;
