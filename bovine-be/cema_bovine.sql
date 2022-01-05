128e2e081f8328d2ed28898fc1b7014cd491dcd2
Merlin Nuñez <merlinsn@gmail.com>
2022-01-04 21:07:28 -0300

    CM-229 Agregar operation_id al bovino

--- bovine-be/cema_bovine.sql
+++ bovine-be/cema_bovine.sql
@@ -4,14 +4,17 @@ create table cema_bovine
 		constraint cema_bovine_pkey
 			primary key,
 	tagging_date timestamp default now(),
-	birth_date timestamp default now(),
 	tag varchar(20) not null,
 	description varchar(250),
-	category varchar(50) default 'Ternero'::character varying not null,
-	status varchar(50) default 'Mamando'::character varying not null
 	genre varchar(10) not null,
-	establishment_cuig varchar(50) not null
+	establishment_cuig varchar(50),
+	birth_date date default now(),
+	category varchar(50) default 'Ternero'::character varying not null,
+	status varchar(50) default 'Mamando'::character varying not null,
+	operation_id uuid
 );
 
 alter table cema_bovine owner to bovine;
+
+
 
