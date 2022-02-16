3303fddf2be2acef2740f1e9ae052c4c014c7ba7
Merlin Nuñez <merlinsn@gmail.com>
2022-02-16 15:04:23 -0300

    CM-280 Agregar worker_username to activity tables

--- activity-be/feeding.sql
+++ activity-be/feeding.sql
@@ -7,7 +7,8 @@ create table feeding
 	name varchar(50) not null,
 	execution_date date default now(),
 	establishment_cuig varchar(50) not null,
-	bovine_tag varchar(50) not null
+	bovine_tag varchar(50) not null,
+	worker_username varchar(50)
 );
 
 alter table feeding owner to activity;
